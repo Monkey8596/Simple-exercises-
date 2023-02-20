a3fe5b89e58ff9b5ccca06128f114b7e7721bde0
Sayin <luffyp2001@gmail.com>
2023-02-19 22:21:38 -0500

    Simple Multiplication

--- array.py
+++
@@ -1,5 +0,0 @@
-def check(seq, elem):
-
-    return elem in seq
-
-print(check(['hola','como','estas'],'scasc'))
---
+++ array_1.py
@@ -0,0 +1,4 @@
+def check(seq, elem):
+    return elem in seq
+
+print(check(['hola','como','estas'],'scasc'))
